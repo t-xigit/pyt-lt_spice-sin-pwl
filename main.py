bf81c29761b2fd984cc43759a7e2da7e55bdfa0d
timur_hp <timur@xigit.de>
2018-06-06 22:07:32 +0200

    git add ma

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
-import matplotlib
+import matplotlib.pyplot as plot
+
+X = [590,540,740,130,810,300,320,230,470,620,770,250]
+Y = [32,36,39,52,61,72,77,75,68,57,48,48]
+
+
+def plot_tutorial():
+    plot.scatter(X,Y)
+    plot.show()
 
 def main():
     print ("Starting main()")
+    plot_tutorial()
     
 main()
