0f5c12a88df04c2ead3d9d93fe2affc7e1df4201
timur_hp <timur@xigit.de>
2018-06-06 22:20:11 +0200

    - sinus example added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plot
+import numpy as np
 
 X = [590,540,740,130,810,300,320,230,470,620,770,250]
 Y = [32,36,39,52,61,72,77,75,68,57,48,48]
@@ -17,8 +18,21 @@ def plot_tutorial():
 
     plot.show()
 
+def plot_sinus_tutorial():
+
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2*np.pi*t)
+    plot.plot(t, s)
+
+    plot.xlabel('time (s)')
+    plot.ylabel('voltage (mV)')
+    plot.title('About as simple as it gets, folks')
+    plot.grid(True)
+    plot.savefig("test.png")
+    plot.show()
+
 def main():
     print ("Starting main()")
-    plot_tutorial()
+    plot_sinus_tutorial()
     
 main()
