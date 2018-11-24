9a9a2f8c5d79da46dc100b830cac4a407347241f
tigit <timur@xigit.de>
2018-11-24 14:46:47 +0100

    - moved everything into one class

--- main.py
+++ main.py
@@ -6,230 +6,177 @@ import datetime
 import time
 from prettytable import PrettyTable
 
-X = [590,540,740,130,810,300,320,230,470,620,770,250]
-Y = [32,36,39,52,61,72,77,75,68,57,48,48]
-#amount of points within one full cycle
-sample_points = 6
 Pi = math.pi
-Sin_Frequenzy = 50
 
-def plot_tutorial():
-    plot.scatter(X,Y, s=60, c='red', marker='^')
+class SinSignal:
 
-    plot.title('Relationship Between Temperature and Iced Coffee Sales')
-    plot.xlabel('Cups of Iced Coffee Sold')
-    plot.ylabel('Temperature in Fahrenheit')
+    # Class Attribute
+    typ = 'sinus'
 
-    plot.xlim(0,1000)
-    plot.ylim(0,100)
+    # Initializer / Instance Attributes
+    def __init__(self, frequenzy, sample_points):
+        self.frequenzy = int(frequenzy)
+        self.sample_points = int(sample_points)
+        self.sp_degree = np.arange(0, sample_points, 1)
+        self.sp_radian = np.arange(0, sample_points, 1)
+        self.sp_normalized = np.arange(0, sample_points, 1)
+        self.sp_percent = np.arange(0, sample_points, 1)
+        self.sp_sin_rad =  np.arange(0, sample_points, 1)
+        self.sp_on_time  = np.arange(0, sample_points, 1)
+        self.sp_off_time  = np.arange(0, sample_points, 1)
+        #the time of one pulse equals to duty cycle 100%
+        self.period = 1/self.frequenzy
+        self.pulse_time = self.period / self.sample_points
+    #Calculates the Sample Points in Rad and Degree
+    def calculate_sample_points(self):
+        print("Sample Points calculated: ", str(self.sample_points))
+        #Rad per Step
+        angle_rad = 2*Pi/self.sample_points
+        print("Angle between Sample Points in Rad ", "{0:.4f}".format(angle_rad))
+        #Degree per Step
+        angle_deg = 360/self.sample_points
+        print("Angle between Sample Points in Degree ", "{0:.2f}".format(angle_deg))
 
-    plot.show()
+        print("Calculating Sample Points in Degree")
+        self.sp_degree = np.arange(0, int(math.degrees(2*Pi)), angle_deg)
+        print("Calculating Sample Points in Rad")
+        self.sp_radian = np.arange(0, 2*Pi, angle_rad)
 
-def plot_sinus_tutorial():
+        if self.sp_radian.size != self.sp_degree.size:
+            print('You fucked up stupid np.arrange failed')
+            exit()
 
-    t = np.arange(0.0, 2.0, 0.01)
-    s = 1 + np.sin(2*np.pi*t)
-    plot.plot(t, s)
+        #Calculate Y Axis Values
+        self.sp_sin_rad = np.sin(self.sp_radian)
+        print("Calculating Normalized Sample Points")
+        #Don't want to have negative values therefore lift the sinwave by 1
+        self.sp_normalized = 1 + self.sp_sin_rad
+        print("Calculating Sample Points in %")
+        self.sp_percent = self.sp_normalized / 2
+        print("Calculating On Time for Pulses")
+        on_time  = self.pulse_time * self.sp_percent
+        print("Calculating Off Time for Pulses")
+        off_time = self.pulse_time - on_time 
 
-    plot.xlabel('time (s)')
-    plot.ylabel('voltage (mV)')
-    plot.title('Tutorial')
-    plot.grid(True)
-    plot.savefig("test.png")
-    plot.show()
+    def plot_sample_points(self):
 
-def plot_sinus(steps):
+        print("Ploting Table: ", str(self.sample_points))
 
-    now = datetime.datetime.now()
-    t = np.arange(0.0, 2*Pi, steps)
-    s = 1 + np.sin(t)
-    print(s)
-    t_ref = np.arange(0.0, 2*Pi, 0.1)
-    s_ref = 1 + np.sin(t_ref)
+        #Doing some logic here to have prettier prints
+        time_unit_multiplier = 1
+        period_unit_multiplier = 1
+        TimeUnitPeriod = "s"
+        TimeUnitPulse  = "s"
 
-    ratio = s/2
-    print(ratio)
+        if self.frequenzy <= 1000:
+                #Frequenzy Below 1KHz
+                time_unit_multiplier = 1000
+                period_unit_multiplier = 1000
+                TimeUnitPeriod = "ms"
+                TimeUnitPulse  = "us"
 
-    plot.plot(t_ref, s_ref)
-    plot.scatter(t,s, s=60, c='red', marker='^')
+        index = 0
 
-    plot.xlabel('time (s)')
-    plot.ylabel('voltage (mV)')
-    plot.title('About as simple as it gets -' + now.strftime("%d-%m-%Y  %H:%M"))
-    plot.grid(True)
-    plot.savefig("test.png")
-    plot.show()
+        x = PrettyTable()
+        x.field_names = ["Sample Points", str(self.sample_points)]
+        x.add_row(["Period in "+ TimeUnitPeriod, " {0:.2f}" .format(self.period*period_unit_multiplier )])
+        x.add_row(["Pulse time in "+ TimeUnitPeriod, " {0:.2f}" .format(self.pulse_time*time_unit_multiplier )])
+        print(x)
 
-def plot_table(sample_points):
+        OnTimeHeader = "On Time in " + TimeUnitPulse
+        OffTimeHeader = "Off Time in " + TimeUnitPulse
+        index = 0
+        y = PrettyTable()
+        y.field_names = ["Degree", "Radian", "sine(Radian)", "Normalized", "Percent", OnTimeHeader, OffTimeHeader]
 
-    print("Sample Points calculated: ", str(sample_points))
-    #Rad per Step
-    angle_rad = 2*Pi/sample_points
-    print("Angle between Sample Points in Rad ", "{0:.4f}".format(angle_rad))
-    #Degree per Step
-    angle_deg = 360/sample_points
-    print("Angle between Sample Points in Degree ", "{0:.2f}".format(angle_deg))
+        for x in np.nditer(self.sp_degree):
+            y.add_row(["{0:.2f}".format(self.sp_degree[index]),
+            "{0:.2f}".format(self.sp_degree[index]),
+            "{0:.2f}".format(self.sp_radian[index]),
+            "{0:.2f}".format(self.sp_normalized[index]),
+            "{0:.1f}".format((self.sp_percent[index])*100),
+            "{0:.8f}".format(self.sp_on_time[index]* time_unit_multiplier),
+            "{0:.8f}".format(self.sp_off_time[index]* time_unit_multiplier)])
 
-    degree = np.arange(0, int(math.degrees(2*Pi)), angle_deg)
-    radian = np.arange(0, 2*Pi, angle_rad)
-    sin_rad = np.sin(radian)
+            index += 1
 
-    if radian.size != degree.size:
-        print('You fucked up stupid np.arrange failed')
-        exit()
+        print('Calculated at - ' + datetime.datetime.now().strftime("%d-%m-%Y  %H:%M"))
+        print(y)
 
-    #Don't want to have negative values therefore lift the sinwave by 1
-    normalized = 1 + sin_rad
-    percent = normalized / 2
+    def plot_OnTime(self):
 
-    #the time of one pulse equals to duty cycle 100%
-    period = 1/Sin_Frequenzy
-    pulse_time = period / sample_points
+        print("Plot OnTime")
 
-    #Doing some logic here to have prettier prints
-    time_unit_multiplier = 1
-    period_unit_multiplier = 1
-    TimeUnitPeriod = "s"
-    TimeUnitPulse  = "s"
+        sample_time = (self.sp_radian/2*Pi)*self.period
 
-    if Sin_Frequenzy <= 1000:
-            #Frequenzy Below 1KHz
-            time_unit_multiplier = 1000
-            period_unit_multiplier = 1000
-            TimeUnitPeriod = "ms"
-            TimeUnitPulse  = "us"
+        #Doing some logic here to have prettier prints
+        time_unit_multiplier = 1
+        period_unit_multiplier = 1
+        TimeUnitPeriod = "s"
+        TimeUnitPulse  = "s"
 
-    on_time  = pulse_time * percent 
-    off_time = pulse_time - on_time 
+        if self.frequenzy <= 1000:
+                #Frequenzy Below 1KHz
+                time_unit_multiplier = 1000
+                period_unit_multiplier = 1000
+                TimeUnitPeriod = "ms"
+                TimeUnitPulse  = "us"
 
-    index = 0
+        high_on  = sample_time -(self.sp_on_time/2)
+        high_off = sample_time +(self.sp_on_time/2)
 
-    x = PrettyTable()
-    x.field_names = ["Sample Points", str(sample_points)]
-    x.add_row(["Period in "+ TimeUnitPeriod, " {0:.2f}" .format(period*period_unit_multiplier )])
-    x.add_row(["Pulse time in "+ TimeUnitPeriod, " {0:.2f}" .format(pulse_time*time_unit_multiplier )])
-    print(x)
+        index = 0
 
-    OnTimeHeader = "On Time in " + TimeUnitPulse
-    OffTimeHeader = "Off Time in " + TimeUnitPulse
-    index = 0
-    y = PrettyTable()
-    y.field_names = ["Degree", "Radian", "sine(Radian)", "Normalized", "Percent", OnTimeHeader, OffTimeHeader]
+        x = PrettyTable()
+        x.field_names = ["Sample Points", str(self.sample_points)]
+        x.add_row(["Period in "+ TimeUnitPeriod, " {0:.2f}" .format(self.period*period_unit_multiplier )])
+        x.add_row(["Pulse time in "+ TimeUnitPeriod, " {0:.2f}" .format(self.pulse_time*time_unit_multiplier )])
+        x.add_row(["Frequenzy ", str(self.frequenzy)])
 
-    for x in np.nditer(degree):
-        y.add_row(["{0:.2f}".format(degree[index]),
-        "{0:.2f}".format(radian[index]),
-        "{0:.2f}".format(sin_rad[index]),
-        "{0:.2f}".format(normalized[index]),
-        "{0:.1f}".format((percent[index])*100),
-        "{0:.8f}".format(on_time[index]* time_unit_multiplier),
-        "{0:.8f}".format(off_time[index]* time_unit_multiplier)])
+        print(x)
 
-        index += 1
+        OnTimeHeader = "On Time in " + TimeUnitPulse
+        OffTimeHeader = "Off Time in " + TimeUnitPulse
+        index = 0
+        y = PrettyTable()
+        y.field_names = ["Degree", "Radian", "sine(Radian)", "Normalized", "Percent", 'Sample Time', 'High Time On', 'High Time Off', OnTimeHeader, OffTimeHeader]
 
-    print('Calculated at - ' + datetime.datetime.now().strftime("%d-%m-%Y  %H:%M"))
-    print(y)
+        for x in np.nditer(self.sp_degree):
+            y.add_row(["{0:.2f}".format(self.sp_degree[index]),
+            "{0:.2f}".format(self.sp_radian[index]),
+            "{0:.2f}".format(self.sp_sin_rad[index]),
+            "{0:.2f}".format(self.sp_normalized[index]),
+            "{0:.1f}".format((self.sp_percent[index])*100),
+            "{0:.2f}".format((sample_time[index])*1000),
+            "{0:.2f}".format((high_on[index])*1000),
+            "{0:.2f}".format((high_off[index])*1000),
+            "{0:.8f}".format(self.sp_on_time[index]* time_unit_multiplier),
+            "{0:.8f}".format(self.sp_off_time[index]* time_unit_multiplier)])
 
-def plot_OnTime(sample_points):
+            index += 1
 
-    print("Plot OnTime")
-    print("Sample Points calculated: ", str(sample_points))
-    #Rad per Step
-    angle_rad = 2*Pi/sample_points
-    print("Angle between Sample Points in Rad ", "{0:.4f}".format(angle_rad))
-    #Degree per Step
-    angle_deg = 360/sample_points
-    print("Angle between Sample Points in Degree ", "{0:.2f}".format(angle_deg))
+        print('Calculated at - ' + datetime.datetime.now().strftime("%d-%m-%Y  %H:%M"))
+        print(y)
 
-    degree = np.arange(0, int(math.degrees(2*Pi)), angle_deg)
-    radian = np.arange(0, 2*Pi, angle_rad)
-    sin_rad = np.sin(radian)
+        FileName = 'Frequenzy_'+ str(self.frequenzy) + 'Hz'+'.pwl'
+        print(FileName)
+        with open(FileName,'w') as pwl:
+            pwl.write(str(y))
 
-    if radian.size != degree.size:
-        print('You fucked up stupid np.arrange failed')
-        exit()
+        t = (on_time*1000)
+        s = degree
+        plot.bar(s, t)
 
-    #Don't want to have negative values therefore lift the sinwave by 1
-    normalized = 1 + sin_rad
-    percent = normalized / 2
-
-    #the time of one pulse equals to duty cycle 100%
-    period = 1/Sin_Frequenzy
-    pulse_time = period / sample_points
-
-    sample_time = (radian/2*Pi)*period
-
-    #Doing some logic here to have prettier prints
-    time_unit_multiplier = 1
-    period_unit_multiplier = 1
-    TimeUnitPeriod = "s"
-    TimeUnitPulse  = "s"
-
-    if Sin_Frequenzy <= 1000:
-            #Frequenzy Below 1KHz
-            time_unit_multiplier = 1000
-            period_unit_multiplier = 1000
-            TimeUnitPeriod = "ms"
-            TimeUnitPulse  = "us"
-
-    on_time  = pulse_time * percent 
-    off_time = pulse_time - on_time 
-    high_on  = sample_time -(on_time/2)
-    high_off = sample_time +(on_time/2)
-
-    index = 0
-
-    x = PrettyTable()
-    x.field_names = ["Sample Points", str(sample_points)]
-    x.add_row(["Period in "+ TimeUnitPeriod, " {0:.2f}" .format(period*period_unit_multiplier )])
-    x.add_row(["Pulse time in "+ TimeUnitPeriod, " {0:.2f}" .format(pulse_time*time_unit_multiplier )])
-    x.add_row(["Frequenzy ", str(Sin_Frequenzy)])
-
-    print(x)
-
-    OnTimeHeader = "On Time in " + TimeUnitPulse
-    OffTimeHeader = "Off Time in " + TimeUnitPulse
-    index = 0
-    y = PrettyTable()
-    y.field_names = ["Degree", "Radian", "sine(Radian)", "Normalized", "Percent", 'Sample Time', 'High Time On', 'High Time Off', OnTimeHeader, OffTimeHeader]
-
-    for x in np.nditer(degree):
-        y.add_row(["{0:.2f}".format(degree[index]),
-        "{0:.2f}".format(radian[index]),
-        "{0:.2f}".format(sin_rad[index]),
-        "{0:.2f}".format(normalized[index]),
-        "{0:.1f}".format((percent[index])*100),
-        "{0:.2f}".format((sample_time[index])*1000),
-        "{0:.2f}".format((high_on[index])*1000),
-        "{0:.2f}".format((high_off[index])*1000),
-        "{0:.8f}".format(on_time[index]* time_unit_multiplier),
-        "{0:.8f}".format(off_time[index]* time_unit_multiplier)])
-
-        index += 1
-
-    print('Calculated at - ' + datetime.datetime.now().strftime("%d-%m-%Y  %H:%M"))
-    print(y)
-
-    FileName = 'Frequenzy_'+ str(Sin_Frequenzy) + 'Hz'+'.pwl'
-    print(FileName)
-    with open(FileName,'w') as pwl:
-        pwl.write(str(y))
-
-    t = (on_time*1000)
-    s = degree
-    plot.bar(s, t)
-
-    plot.xlabel('Degree')
-    plot.ylabel('OnTime')
-    plot.title('OnTime')
-    plot.grid(True)
-    plot.show()
+        plot.xlabel('Degree')
+        plot.ylabel('OnTime')
+        plot.title('OnTime')
+        plot.grid(True)
+        plot.show()
 
 def main():
     print ("Starting main()")
-    #plot_sinus_tutorial()
-    #plot_sinus(steps)
-    plot_table(sample_points)
-    plot_OnTime(sample_points)
+    signal = SinSignal(100,128)
+    signal.calculate_sample_points()
+    signal.plot_sample_points()
 
 main()
